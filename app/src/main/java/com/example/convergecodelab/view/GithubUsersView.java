5ea5c7aca4c3e0393e76f2dc0f226495b4432b22
PromasterGuru <pmutondo12@gmail.com>
2019-03-04 16:14:52 +0300

    feat(access-api): Accessing the API
    - Construct a service leveraging the GithubApi interface with the defined endpoints
    
    [Delivers #155931587]

---
+++ app/src/main/java/com/example/convergecodelab/view/GithubUsersView.java
@@ -0,0 +1,9 @@
+package com.example.convergecodelab.view;
+
+import com.example.convergecodelab.model.GithubUsers;
+
+import java.util.List;
+
+public interface GithubUsersView {
+    void githubReadyUsers(List<GithubUsers> users);
+}
