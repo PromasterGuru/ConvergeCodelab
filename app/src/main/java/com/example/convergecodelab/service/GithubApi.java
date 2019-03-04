81af1d0e608c5e15bd525284e2116943e771d8e1
PromasterGuru <pmutondo12@gmail.com>
2019-03-04 11:25:52 +0300

    feat(api-endpoints) : Define the API Endpoints
    - Implement GithubApi interface
    - Add retrofit annotations (@GET) to encode details about the parameters and request method
    - Update the baseURL
    
    [Delivers #155903085]

--- app/src/main/java/com/example/convergecodelab/service/GithubApi.java
+++ app/src/main/java/com/example/convergecodelab/service/GithubApi.java
@@ -1,4 +1,12 @@
 package com.example.convergecodelab.service;
 
-public class GithubApi {
+import com.example.convergecodelab.model.GithubUsersResponse;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+
+public interface GithubApi {
+    @GET("search/users?q=+language:java+location:nairobi")
+    Call<GithubUsersResponse> githubUsersList();
 }
+
