5543a58c934c5343114b5be1826ca736b38fb9c5
PromasterGuru <pmutondo12@gmail.com>
2019-03-06 13:14:40 +0300

    chore (pre-commit-hook):  Implement a pre-commit hook which analyzes the code before commit
    - Checks Java code style violations
    - Checks for Unsuccessful builds
    -Checks for Failing unit tests on the changes made.
    
    [Delivers #155965911]

---
+++ app/src/main/java/com/example/convergecodelab/view/MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.convergecodelab.view;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.RelativeLayout;
+
+import com.example.convergecodelab.R;
+
+public class MainActivity extends AppCompatActivity {
+    RelativeLayout detailsView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        detailsView = (RelativeLayout) findViewById(R.id.detailsView);
+
+        detailsView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                viewDetails();
+            }
+        });
+    }
+
+    /*Launch DetailActivity to view user profile*/
+    public void viewDetails(){
+        Intent intent = new Intent(this, DetailActivity.class);
+        startActivity(intent);
+    }
+
+}
